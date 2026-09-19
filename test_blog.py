df0228e87db9f242a0cd73d895b34c6d9ff99654
kiki <0o.k0i0ki.o0@gmail.com>
2026-09-19 13:44:31 +0900

    Update test_blog.py

--- test_blog.py
+++ test_blog.py
@@ -1,49 +1,83 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from datetime import datetime
+from xml.sax.saxutils import escape
 
-url = "https://sakurazaka46.com/s/s46/diary/detail/70909?ima=0000&cd=blog"
+ARTICLE_URL = (
+    "https://sakurazaka46.com/s/s46/diary/detail/"
+    "70909?ima=0000&cd=blog"
+)
+
+BASE_URL = "https://sakurazaka46.com"
+FEED_URL = "https://example.com/feeds/kojima-nagisa.xml"
 
 headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-res = requests.get(url, headers=headers, timeout=15)
+res = requests.get(ARTICLE_URL, headers=headers, timeout=15)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "html.parser")
 
-print(soup.title.get_text(strip=True))
-
 article_body = soup.find(class_="box-article")
 
-if article_body:
-    for tag in article_body.find_all(["script", "style", "noscript"]):
-        tag.decompose()
+if not article_body:
+    raise RuntimeError("本文エリアが見つかりません")
 
-    for img in article_body.find_all("img"):
-        src = (
-            img.get("src")
-            or img.get("data-src")
-            or img.get("data-original")
-            or ""
-        )
+for tag in article_body.find_all(["script", "style", "noscript"]):
+    tag.decompose()
 
-        if not src:
-            img.decompose()
-            continue
+for img in article_body.find_all("img"):
+    src = (
+        img.get("src")
+        or img.get("data-src")
+        or img.get("data-original")
+        or ""
+    )
 
-        image_url = urljoin("https://sakurazaka46.com", src)
+    if not src:
+        img.decompose()
+        continue
 
-        img["src"] = image_url
-        img["alt"] = img.get("alt") or "小島凪紗 公式ブログ画像"
+    image_url = urljoin(BASE_URL, src)
 
-        link = article_body.new_tag("a", href=image_url)
-        img.wrap(link)
+    img["src"] = image_url
+    img["alt"] = img.get("alt") or "小島凪紗 公式ブログ画像"
 
-    content_html = article_body.decode_contents()
+    link = article_body.new_tag("a", href=image_url)
+    img.wrap(link)
 
-    print("本文HTMLを作成できました")
-    print(content_html[:1000])
-else:
-    print("本文エリアが見つかりません")
+content_html = article_body.decode_contents()
+
+title = soup.title.get_text(" ", strip=True)
+updated = datetime.now().astimezone().isoformat()
+
+feed_xml = f'''<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>櫻坂46｜小島凪紗 公式ブログ</title>
+  <id>{escape(FEED_URL)}</id>
+  <updated>{escape(updated)}</updated>
+  <link href="{escape(FEED_URL)}" rel="self"/>
+
+  <entry>
+    <title>{escape(title)}</title>
+    <id>{escape(ARTICLE_URL)}</id>
+    <link href="{escape(ARTICLE_URL)}"/>
+    <updated>{escape(updated)}</updated>
+    <author>
+      <name>小島凪紗</name>
+    </author>
+    <content type="html"><![CDATA[
+{content_html}
+    ]]></content>
+  </entry>
+</feed>
+'''
+
+with open("kojima-nagisa.xml", "w", encoding="utf-8") as f:
+    f.write(feed_xml)
+
+print("Atomフィードを生成しました")
+print("ファイル: kojima-nagisa.xml")
